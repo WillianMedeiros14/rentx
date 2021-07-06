15e37d2ad9d1438659fa8d4fe2696b4bbc80d814
Willian Medeiros <willianmedeiorslm@gmail.com>
2021-07-06 11:55:20 -0300

    Animações - animando scroll tela CarDetails

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { StatusBar, StyleSheet } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { useTheme } from 'styled-components';
+
+import Animated, {
+    useSharedValue,
+    useAnimatedScrollHandler,
+    useAnimatedStyle,
+    interpolate,
+    Extrapolate
+} from 'react-native-reanimated';
 
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
@@ -15,7 +26,7 @@ import {
     Container,
     Header,
     CarImages,
-    Content,
+    
     Details,
     Description,
     Brand,
@@ -29,6 +40,7 @@ import {
 } from './styles';
 
 
+
 interface Params {
     car: CarDTO;
 }
@@ -37,6 +49,36 @@ export function CarDetails(){
     const navigation = useNavigation();
     const route = useRoute();
     const { car } = route.params as Params;
+
+    const theme = useTheme();
+
+    const scrollY = useSharedValue(0);
+    const scrollHandler = useAnimatedScrollHandler(event => {
+        scrollY.value = event.contentOffset.y;
+    });
+
+    const headerStyleAnimation = useAnimatedStyle(() => {
+        return {
+            height: interpolate(
+                scrollY.value,
+                [0, 200],
+                [200, 70],
+                Extrapolate.CLAMP
+            ),
+        }
+    });
+
+    const sliderCarsStyleAnimation = useAnimatedStyle(() => {
+        return {
+            opacity: interpolate(
+                scrollY.value,
+                [0, 150],
+                [1, 0],
+                Extrapolate.CLAMP
+            )
+        }
+    });
+
 
     function handleConfirmHental(){
         navigation.navigate('Scheduling', { car });
@@ -48,16 +90,40 @@ export function CarDetails(){
     
     return (
         <Container>
-            <Header>
-                <BackButton onPress={handleBack} />
-            </Header>
+            <StatusBar
+                 barStyle="dark-content"
+                 translucent
+                 backgroundColor="transparent"
+            />
 
-            <CarImages>
-                <ImageSlider imagesUrl={car.photos} />
-            </CarImages>
+            <Animated.View
+                style={[
+                    headerStyleAnimation,
+                    styles.header,
+                    { backgroundColor: theme.colors.background_secondary }
+                ]}
+            >
+                <Header>
+                    <BackButton onPress={handleBack} />
+                </Header>
+
+                <Animated.View style={sliderCarsStyleAnimation}>
+                    <CarImages>
+                        <ImageSlider imagesUrl={car.photos} />
+                    </CarImages>
+                </Animated.View>
+            </Animated.View>
             
-            <Content>
-                <Details>
+            <Animated.ScrollView 
+                contentContainerStyle={{
+                    paddingHorizontal: 24,
+                    paddingTop: getStatusBarHeight() + 160
+                }}
+                showsVerticalScrollIndicator={false}
+                onScroll={scrollHandler}
+                scrollEventThrottle={16}
+            >
+                <Details> 
                     <Description>
                         <Brand>{car.brand}</Brand>
                         <Name>{car.name}</Name>
@@ -85,7 +151,8 @@ export function CarDetails(){
                 <About>
                    {car.about}
                 </About>
-            </Content>
+            </Animated.ScrollView>
+
 
             <Footer>
                 <Button title="Escolher príodo do aluguel" onPress={handleConfirmHental}/>
@@ -94,3 +161,11 @@ export function CarDetails(){
         </Container>
     );
 }
+
+const styles = StyleSheet.create({
+    header: {
+        position: 'absolute',
+        overflow: 'hidden',
+        zIndex: 1
+    },
+})
