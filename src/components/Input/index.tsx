86553d713356591c0f2e420fbacda25f4ca7df48
Willian Medeiros <willianmedeiorslm@gmail.com>
2021-07-18 16:38:18 -0300

    Refactor dos Inputs da tela de login

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -36,8 +36,8 @@ export function Input({
 
 
     return (
-        <Container isFocused={isFocused}>
-            <IconContainer>
+        <Container >
+            <IconContainer isFocused={isFocused}>
                 <Feather
                     name={iconName}
                     size={24}
@@ -48,6 +48,7 @@ export function Input({
             <InputText
                 onFocus={handleInputFocus}
                 onBlur={handleInputBlur}
+                isFocused={isFocused}
                 {...rest} 
             />
         </Container>
--- src/components/PasswordInput/index.tsx
+++ src/components/PasswordInput/index.tsx
@@ -42,8 +42,8 @@ export function PasswordInput({
 
 
     return (
-        <Container isFocused={isFocused}>
-            <IconContainer>
+        <Container>
+            <IconContainer isFocused={isFocused}>
                 <Feather
                     name={iconName}
                     size={24}
@@ -55,11 +55,12 @@ export function PasswordInput({
                 secureTextEntry={isPasswordVisible}
                 onFocus={handleInputFocus}
                 onBlur={handleInputBlur}
+                isFocused={isFocused}
                 {...rest} 
             />
 
             <BorderlessButton onPress={handlePasswordVisibilityChange}>
-                <IconContainer>
+                <IconContainer isFocused={isFocused}>
                     <Feather
                         name={isPasswordVisible ? 'eye' : 'eye-off'}
                         size={24}
