f9ad95de941862f6e5a1202f0167ac44410a9bbb
Willian Medeiros <willianmedeiroslm@gmail.com>
2022-01-08 16:26:50 -0300

    refactor: memory leak

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -66,18 +66,27 @@ export function Home(){
     // })
     
     useEffect(() => {
+        let isMounted = true;
+
         async function fetchCars(){
             try {
                 const response = await api.get('/cars');
-                setCars(response.data);
+                if(isMounted){
+                    setCars(response.data);
+                }
             }catch(error){
                 console.log(error);
             }finally{
-                setLoading(false);
+                if(isMounted){
+                    setLoading(false);
+                }
             }
         }
 
-        fetchCars()        
+        fetchCars();
+        return () => {
+            isMounted = false;
+        };
     },[]);
 
     function handleCarDetails(car: CarDTO){
